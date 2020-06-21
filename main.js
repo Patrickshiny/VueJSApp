b7a3373f98bf4452c8bee91ef68ceeafc0b4d59a
Patrickshiny <s710482303@gm.ntpu.edu.tw>
2020-06-22 01:13:34 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 requestAnimationFrame();
 require('/here');
 require('/essential');
+require('/preset');
+
+DataBackup();
+DataStoring();
+
+Refresh ();
+AppInit ();
 
 require('/Dataset');
 require('/Configuration');
-require('/preset');
 
 renderFromPreset();
 renderFromData();
 renderFromFinishingSetting();
 
-AppInit ();
-AppFinished ();
-DataBackup();
-DataStoring();
 
+AppFinished ();
 ClosingGUI();
+
